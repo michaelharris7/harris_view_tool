d56dcfb3ab04aee9d1cbe7803786c36c2232daf3
Michael Harris <michaeljh7@gmail.com>
2018-01-03 12:25:53 -0700

    Harris tool initial commit

---
+++ lib/harris_view_tool.rb
@@ -0,0 +1,6 @@
+require "harris_view_tool/version"
+require "harris_view_tool/renderer"
+
+module HarrisViewTool
+  # Your code goes here...
+end
---
+++ lib/harris_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HarrisViewTool
+  VERSION = "0.1.0"
+end
